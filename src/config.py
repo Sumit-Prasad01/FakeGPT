32501d1f9f5ab37896298d794e289b6b5c9c50ed
Sumit prasad <sumitprasadd.wb24@gmail.com>
2026-07-18 21:39:21 +0530

    add config and agent

---
+++ src/config.py
@@ -0,0 +1,24 @@
+import os
+import certifi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    GOOGLE_MODEL = os.getenv("GOOGLE_MODEL", "gemini-2.5-flash")
+
+    TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+
+    LANGSMITH_TRACING = os.getenv("LANGSMITH_TRACING")
+    LANGSMITH_ENDPOINT = os.getenv("LANGSMITH_ENDPOINT")
+    LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+    LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT")
+
+    os.environ["SSL_CERT_FILE"] = certifi.where()
+    os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+
+Settings = Settings()
